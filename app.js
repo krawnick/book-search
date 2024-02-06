921ebbc3ba3090a199ef1cfdd33a429966426856
krawnick <kraw.nick@gmail.com>
2024-02-06 16:32:24 +0500

    fix Map, Set

--- app.js
+++ app.js
@@ -1,13 +1,1 @@
 'use strict'
-
-let objA = { a: 1 }
-let objB = { b: 2 }
-
-const set = new WeakSet([objA, objB])
-console.log('set', set)
-
-objA = null
-
-setTimeout(() => {
-  console.log('1000ms set', set)
-}, 1000)
