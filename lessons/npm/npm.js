ab95780026074d5be047718b14278a88636a17eb
krawnick <kraw.nick@gmail.com>
2024-02-17 17:10:13 +0500

    npm

--- lessons/npm/npm.js
+++ lessons/npm/npm.js
@@ -1,5 +1,15 @@
-npm -v
-npm who am i 
-npm profile get
-npm login  
-npm  profile set fullname "Nikita Kravtsov" 
+// npm -v
+// npm who am i
+// npm profile get
+// npm login
+// npm  profile set fullname "Nikita Kravtsov"
+
+// npm search "keywords" // поиск пакетов по ключ-слову
+// npm fund
+// npm dedub
+// npm cache verify // если что-то не устанавливается
+
+// npm i
+// npm uni
+// npm i -g // глобальная установка
+// npm i -D // установка пакета для разработки приложения
